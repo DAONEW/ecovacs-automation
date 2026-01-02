ea4c8cb8f8bb37af392002ea1d8f6bcf337e0094
DAONEW <mail@leo-becker.de>
2026-01-02 15:37:56 +0100

    sec_dummy

---
+++ sec_DUMMY.py
@@ -0,0 +1,11 @@
+# MQTT settings
+"MQTT_PORT": None
+"MQTT_BROKER": None
+"MQTT_USER": None
+"MQTT_PASSWORD": None
+
+# Home Assistant discovery prefix
+"HA_DISCOVERY_PREFIX": None
+
+# Device display name
+"DEVICE_NAME": None
